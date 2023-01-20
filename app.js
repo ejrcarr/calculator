7c190f91457344ad2c35011d94d83c05b8260740
Evan Carr <ejrcarr@gmail.com>
2023-01-20 11:10:02 -0500

    Created operate function

--- app.js
+++ app.js
@@ -8,8 +8,28 @@ const zeroButton = document.querySelector('.zero-button');
 const changeSignsButton = document.querySelector('.change-sign-button');
 
 const MAX_LENGTH = 10;
-const OPERATORS = new Set(['+', '-', '÷', 'x']);
+const OPERATORS = new Set(['+', '-', '÷', '×']);
 const NUMBERS = new Set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
+
+let leftNumber = null;
+let currentOperator = null;
+let rightNumber = null;
+
+let lastAnswer = null;
+
+equalsButton.addEventListener('click', () => {
+	rightNumber = parseFloat(result.value.trim());
+	console.log(`${leftNumber} ${currentOperator} ${rightNumber}`);
+	operate(currentOperator, leftNumber, rightNumber);
+	currentOperator = null;
+});
+
+operatorButtons.forEach((operator) => {
+	operator.addEventListener('click', () => {
+		leftNumber = parseFloat(result.value.trim());
+		currentOperator = operator.textContent;
+	});
+});
 
 changeSignsButton.addEventListener('click', () => {
 	let trimmedContent = result.value.trim();
@@ -22,6 +42,8 @@ changeSignsButton.addEventListener('click', () => {
 
 clearButton.addEventListener('click', () => {
 	result.value = '0';
+	currentOperator = null;
+	lastAnswer = null;
 });
 
 decimalButton.addEventListener('click', () => {
@@ -31,7 +53,9 @@ decimalButton.addEventListener('click', () => {
 });
 
 zeroButton.addEventListener('click', () => {
-	if (result.value.trim() !== '0' && result.value.length <= MAX_LENGTH) {
+	if (currentOperator !== null) {
+		result.value = '0';
+	} else if (result.value.trim() !== '0' && result.value.length <= MAX_LENGTH) {
 		result.value += '0';
 	}
 });
@@ -39,8 +63,8 @@ zeroButton.addEventListener('click', () => {
 numberButtons.forEach((button) => {
 	button.addEventListener('click', () => {
 		let currText = result.value.trim();
-		// let lastCharacter = currText.charAt(result.textContent.length-1);
-		if (currText === '0') {
+		console.log(currentOperator);
+		if (currText === '0' || currentOperator !== null || lastAnswer !== null) {
 			result.value = button.textContent;
 		} else if (result.value.length <= MAX_LENGTH) {
 			result.value += button.textContent;
@@ -65,4 +89,34 @@ function isAllNumbers(values) {
 	return true;
 }
 
-function operate(operator, leftTerm, rightTerm) {}
+function operate(operator, leftTerm, rightTerm) {
+	let answer = 0;
+	console.log(`${leftTerm} ${operator} ${rightTerm}`);
+	switch (operator) {
+		case '×':
+			answer = leftTerm * rightTerm;
+			break;
+		case '÷':
+			if (rightTerm === 0) {
+				result.value = `Can't do that`;
+				return;
+			}
+			answer = leftTerm / rightTerm;
+			break;
+		case '+':
+			answer = leftTerm * rightTerm;
+			break;
+		case '-':
+			answer = leftTerm - rightTerm;
+			break;
+		default:
+			return;
+	}
+	answer = parseFloat(answer).toLocaleString('en-US', {
+		maximumFractionDigits:
+			MAX_LENGTH - answer.toString().split('.')[0].length - 1,
+		minimumFractionDigits: 0,
+	});
+	lastAnswer = answer;
+	result.value = answer;
+}
